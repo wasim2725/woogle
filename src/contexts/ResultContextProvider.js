78caa0b7282a03b20d21c97328d4e685393d87fe
Mohd Wasim <mohdwasim368@gmail.com>
2022-08-02 17:31:33 +0530

    Initial commit

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+    console.log("added at line no 13");
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: 'GET',
+      headers: {
+        'x-user-agent': 'desktop',
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': '5d5281a00fmshddab72759decf34p1e6dbfjsn372984a113ff',
+      },
+    });
+    console.log("line no 22");
+    const data = await response.json();
+
+    console.log(data);
+
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import { ResultContextProvider } from './contexts/ResultContextProvider';
+import './global.css';
+
+ReactDom.render(
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
+  document.getElementById('root'),
+);
